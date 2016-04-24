8594e60b912c553159428ed9040848a1c1a994e1
martynas <mj@svift.net>
2016-04-24 19:53:37 +0200

    finished game

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -7,12 +7,54 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+
+    @IBAction func buttonTapped(sender: UIButton) {
+        if (sender.tag == correctAnswer) {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .Alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .Default, handler: askQuestion))
+        presentViewController(ac, animated: true, completion: nil)
+    }
+    
+    var score = 0
+    var correctAnswer = 0
+    var countries = ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let buttons = [button1, button2, button3]
+        
+        for button in buttons {
+            button.layer.borderWidth = 1
+            button.layer.borderColor = UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 1).CGColor
+        }
+        
+        askQuestion()
+    }
+    
+    func askQuestion(action: UIAlertAction! = nil) {
+        // shuffle the array and save it
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(countries) as! [String]
+        
+        button1.setImage(UIImage(named: countries[0]), forState: .Normal)
+        button2.setImage(UIImage(named: countries[1]), forState: .Normal)
+        button3.setImage(UIImage(named: countries[2]), forState: .Normal)
+        
+        correctAnswer = GKRandomSource.sharedRandom().nextIntWithUpperBound(3)
+        self.title = countries[correctAnswer].uppercaseString
     }
 
     override func didReceiveMemoryWarning() {
